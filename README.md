2e298b0507f069b0deba88d1b052a9ce75d9d60b
Hanul <hanul@hanul.me>
2021-10-27 16:08:59 +0900

    dev done

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dogesound-compilation-mint-contracts
 
 ## Polygon
-- CompMint: [0x706Fc0944cBF26a4322f3FB89228214Ea7Cc965C](https://polygonscan.com/address/0x706Fc0944cBF26a4322f3FB89228214Ea7Cc965C)
+- CompMint: [0x40EC6F393163B5eB5fD76AE10860644e0f11411c](https://polygonscan.com/address/0x40EC6F393163B5eB5fD76AE10860644e0f11411c)
 
 ## License
 [MIT](LICENSE)
--- contracts/CompMint.sol
+++ contracts/CompMint.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "./interfaces/ICompMint.sol";
 import "./interfaces/IFungibleToken.sol";
 
-contract CompMint is Ownable, ICompMint {
+contract CompMint is Ownable, ICompMint, IERC1155Receiver, ERC165 {
 
     IERC1155 public nft;
     IFungibleToken public pmix;
@@ -29,5 +31,29 @@ contract CompMint is Ownable, ICompMint {
     ) override external {
         pmix.permit(msg.sender, address(this), 10 ether, deadline, v, r, s);
         mint(id);
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+        return interfaceId == type(IERC1155).interfaceId || super.supportsInterface(interfaceId);
     }
 }
